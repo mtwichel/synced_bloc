7767aaf341f6473faad81af5c6f2aead92760b8f
Marcus Twichel <marc.twichy@gmail.com>
2025-07-26 14:47:01 -0600

    Remove build directory from tracking

--- example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/marcustwichel/Developer/Playing_Around/synced_bloc/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/marcustwichel/Developer/Playing_Around/synced_bloc/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
